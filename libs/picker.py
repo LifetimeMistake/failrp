7d6e0f0c3a5711c94e8b39929a4619859c2f4744
PiRifle <57302468+PiRifle@users.noreply.github.com>
2023-04-02 23:26:50 +0200

    added picker

--- libs/picker.py
+++ libs/picker.py
@@ -9,10 +9,11 @@ def pick_from_list(lst, default=0, timeout=None):
     curses.noecho()
     curses.cbreak()
     stdscr.keypad(True)
-
+    h, w = 0,0
     # Set up non-blocking input mode
     stdscr.nodelay(1)
-
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     try:
         result = lst[default]
         selected = 0
@@ -20,13 +21,17 @@ def pick_from_list(lst, default=0, timeout=None):
 
         stdscr.clear()
         while True:
+            if h != stdscr.getmaxyx()[0] or w != stdscr.getmaxyx()[1]:
+                stdscr.clear()
+            h, w = stdscr.getmaxyx()
             for i, item in enumerate(lst):
                 if i == selected:
-                    stdscr.addstr(i, 0, f"> {item}")
+                    stdscr.addstr(i+ h//2, w//2-len(item)//2, f"> {item}", curses.color_pair(1))
                 else:
-                    stdscr.addstr(i, 0, f"  {item}")
-            timing = f"(Default: {lst[default]})\nDefault will be picked in {int(5 - (time.monotonic() - start_time))} seconds" if not interrupted else f"You Picked Option {lst[selected]}\n\n"
-            stdscr.addstr(len(lst) + 1, 0, f"\n"+timing)
+                    stdscr.addstr(i+ h//2, w//2-len(item)//2, f"  {item}")
+            timing = f"(Default: {lst[default]}) Default will be picked in {int(5 - (time.monotonic() - start_time))} seconds" if not interrupted else f"{' '*20}You Picked {lst[selected]}{' '*20}"
+            stdscr.addstr(len(lst) + 1 + h//2, w//2-len(timing)//2, timing)
+
             # Check if timeout has elapsed
             if timeout is not None and time.monotonic() - start_time >= timeout and not interrupted:
                 result = lst[default]
@@ -57,12 +62,15 @@ def pick_from_list(lst, default=0, timeout=None):
         curses.echo()
         curses.endwin()
 
-options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
-default = 4
-timeout = 5
+class AnsiPicker:
+    def __init__(self, options: "dict[str, any]"):
+        self.options = options
+    def ask(self, timeout=10, default_key = None):
+        if not default_key:
+            idx = 0
+        else:
+            idx = list(self.options.keys()).index(default_key)
+        out = pick_from_list(list(self.options.keys()), idx, timeout)
+        return self.options[out]
 
-print("Use arrow keys to navigate, Enter to confirm. Press Ctrl-C to exit.")
 
-result = pick_from_list(options, default=default, timeout=timeout)
-
-print(f"\nYou picked: {result}")
